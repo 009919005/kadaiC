a837ff43d3b62da8d2f488b2b52c623a37ae176d
19005 <113569187+009919005@users.noreply.github.com>
2022-10-27 14:10:05 +0900

    Update clock.js

--- clock.js
+++ clock.js
@@ -5,4 +5,13 @@ const clock=()=>{
   let min=d.getMinutes();
   let sec=d.getSeconds();
   
-  hour=
+  hour=hour<10?"0"+hour:hour;
+  min=min<10?"0"+min:min;
+  sec=sec<10?"0"+sec:sec;
+  
+  let time='${hour}:${min}:${sec}';
+  
+  document.querySelector(".clock-time").innerText=time;
+};
+
+setInterval(clock,1000}
